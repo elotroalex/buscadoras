38c410e09e707740cf5e02f04abf86dc8df3ede4
morphogonia <sandrax@bulabe.com>
2021-11-16 15:10:30 -0600

    errores

--- assets/bus.js
+++ assets/bus.js
@@ -53,8 +53,7 @@ jQuery(function($) {
 $(document).ready(function() {
 
 
-  $('.navi').click(function() {
-
+  $('.nav-menu li').click(function() {
     var elementClicked = $(this).attr('href');
     if (elementClicked=="#home") {
       var destination =0;
@@ -64,6 +63,39 @@ $(document).ready(function() {
     $('html:not(:animated),body:not(:animated)').animate({ scrollTop: destination}, 700);
     return false;
   });
+
+
+
+
+  if($(window).width() < 640){
+    var view = "80%";
+  } else { view = "280px" }
+
+
+  var turn = 1;
+  $('.hamburger').click(function() {
+      $(this).toggleClass("activo");
+      if (turn==1) {
+        $(".nav-menu").fadeToggle("slow", function(){
+          $(".nav-p, #bus").hide();
+          $("sidebar").animate({
+            width: "80px",
+          }, 1200, function(){
+            turn=0;
+          });
+        });
+
+      } else if (turn==0) {
+        $("sidebar").animate({
+          width: "view",
+        }, 900, function(){
+          $(".nav-menu").fadeToggle("slow", function(){
+            turn=1;
+            $(".nav-p, #bus").fadeIn(600);
+          });
+        });
+      }
+  }); /* / hamburger */
 
 
 
